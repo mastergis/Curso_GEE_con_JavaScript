b260909ecdfa1550044e68652c0a7d701ddb2758
mastergis <60658810+mastergis@users.noreply.github.com>
2021-08-11 13:10:44 -0500

    Update 01VarType.js
    script

--- Modulo02/01VarType.js
+++ Modulo02/01VarType.js
@@ -1,1 +1,21 @@
+//Mi primer Script con Google Earth engine en JS
+
+var myGreeting = "Hello World"
+print(myGreeting)
+
+//..........Tipo de datos en JS....................................
+var Edad = 42; // Edad es un Number
+var Nombre= "Jose"; // Nombre un String
+var foo = true; // foo un Boolean
+
+//..........Tipo de datos en C#....................................
+/*  
+int edad = 38;
+string nombre = "Marcel";
+bool b = false;
+*/
+
+print(Edad)
+
+console.log(foo)
 
