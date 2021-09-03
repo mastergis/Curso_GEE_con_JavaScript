759de12a828d12fc484f78b77c4352b09f868b07
mastergis <60658810+mastergis@users.noreply.github.com>
2021-09-03 10:36:31 -0500

    Create 02AlgMaps.js
    code

---
+++ Modulo04/02AlgMaps.js
@@ -0,0 +1,21 @@
+//Matematica de bandas 
+var colec = ee.ImageCollection("LANDSAT/LC08/C01/T1_TOA")
+            .filterBounds(geometry)
+            .filterDate('2020-01-01', '2020-05-20')
+            .filterMetadata('CLOUD_COVER', 'less_than', 5);
+print(colec);
+
+var img = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_093077_20200427')
+
+Map.addLayer(img, {min:0.0, max:0.5, bands:['B6','B5', 'B4']}, 'Landsat 8')
+
+//Calculo del NBR (NIR-SWIR/NIR+SWIR)
+var NBR = img.select('B5').subtract(img.select('B7'))
+          .divide(img.select('B5').add(img.select('B7')));
+
+var NBR2 = img.expression('float(NIR-SWIR)/float(NIR+SWIR)', {
+  'NIR':img.select('B5'),
+  'SWIR':img.select('B6')
+});
+Map.addLayer(NBR, {min:0.0, max:1, 'palette': ['#7F0010', '#D99143', '#C04529', '#E02E20', '#EC6521', '#F6D53B']}, 'NBR')
+Map.addLayer(NBR2, {min:0.0, max:1}, 'NBR2', false)
